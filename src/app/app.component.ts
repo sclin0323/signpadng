33be728697669a8a998247769bbbce57745ea115
sclin0323 <sclin0323@gmail.com>
2018-05-01 13:27:45 +0800

    增加Bootstrap 簽名板

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SignaturePad } from 'angular2-signaturepad/signature-pad';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  
+  @ViewChild(SignaturePad) signaturePad: SignaturePad;
+
+  // option 可參考: https://www.npmjs.com/package/signature_pad
+  private signaturePadOptions: Object = {
+    //'canvasWidth': 250,   // 不固定SIZE
+    //'canvasHeight': 200,  // 不固定SIZE
+    'backgroundColor': 'rgba(225, 255, 255, 0)',  // 筆尖的顏色
+    'penColor': 'rgb(0, 0, 0)'  // 筆跡顏色
+  };
+  
   title = 'app';
+
+  constructor() {
+    // no-op
+  }
+ 
+  ngAfterViewInit() {
+    // this.signaturePad is now available
+    this.signaturePad.set('maxWidth', 2);
+    this.signaturePad.set('minWidth', 1); // set szimek/signature_pad options at runtime
+    this.signaturePad.clear(); // invoke functions from szimek/signature_pad API
+  }
+ 
+  drawComplete() {
+    // will be notified of szimek/signature_pad's onEnd event
+    console.log(this.signaturePad.toDataURL());
+  }
+ 
+  drawStart() {
+    // will be notified of szimek/signature_pad's onBegin event
+    console.log('begin drawing');
+  }
+
+  // 清除簽名版
+  clear() {
+    // will be notified of szimek/signature_pad's onBegin event
+    console.log('clear');
+    this.signaturePad.clear()
+  }
+
+  saveButton() {
+    if (this.signaturePad.isEmpty()) {
+      alert("Please provide a signature first.");
+    } else {
+      const data = this.signaturePad.toData();
+      console.log(data);
+
+      //window.URL.revokeObjectURL();
+    }
+  }
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { SignaturePadModule } from 'angular2-signaturepad';
+
+import {MatButtonModule, MatCheckboxModule, MatInputModule} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule, 
+    SignaturePadModule, 
+    BrowserAnimationsModule, 
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatRadioModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
